c9600328125ab9de1cf22d7309c362ff355461d3
Sourabh <sourabh8393@gmail.com>
2014-02-21 08:51:09 +0530

    ssk

--- erecruitment/app/controllers/testpaper_controller.rb
+++ erecruitment/app/controllers/testpaper_controller.rb
@@ -3,8 +3,8 @@ class TestpaperController < ApplicationController
   	#@my_questions=My_question.paginate(:page => params[:page], :per_page => 1).order(:created_by)
   	#@temp=My_question.find_by_question_type("demo").paginate(:page => params[:page], :per_page => 1)
   	@marks_obtained=0
+  	@user=User.find(session[:user_id])
     test_type=params[:test_owner]
-    @user=User.find(session[:id])
   	sql="select * from my_questions where created_by='"+test_type+"'"
     @temp = My_question.paginate_by_sql(sql, :page =>params[:page], :per_page => 1,  :previous_label => '')
     end
